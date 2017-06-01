1d84394f27eace7bdd0f914032dc062db4661ad1
LonSilent <101703044@nccu.edu.tw>
2017-06-01 16:23:10 +0800

    merge features

--- predict.py
+++ predict.py
@@ -27,6 +27,7 @@ if __name__ == '__main__':
     offline_test = False
     method_set = ['LR', 'RF', 'GBDT', 'ADA', 'XGB']
     method = method_set[4]
+    train_skip_row = [3306, 13016, 17682, 20072, 20794, 23305, 23884, 25228, 25315, 39769, 44619, 46596, 47056, 51909, 57484, 63712, 72844, 74304, 86457, 96725, 102512, 104101, 105780, 106577, 108978, 109009, 109311, 115347, 130637, 134403, 139219, 141281, 144506, 151922, 158778, 164553, 169290, 175282, 180461, 181695, 182943, 189396, 189659, 190570, 193246, 193815, 198913, 199110, 201841, 205947, 208199, 208485, 208798, 213220, 216861, 226925, 231151, 231313, 231879, 236655, 245880, 248125, 250701, 254161, 257077, 260779, 263134, 270146, 273065, 289307, 297461, 301583, 306878, 312498, 322705, 324777, 325200, 325530, 326297, 327206, 328601, 328745, 351788, 357127, 361480, 363362, 365317, 381124, 384293, 402423]
 
     train_path = base + 'train_features.pickle'
     test_path = base + 'test_features.pickle'
@@ -38,6 +39,9 @@ if __name__ == '__main__':
     use_deep_bm25 = True
     use_lstm = False
     use_magic_v2 = True
+    use_new = False
+    use_word_count = False
+    use_best = True
 
     if use_vsm:
         train_vsm = base + 'train_vsm.pickle'
@@ -66,6 +70,18 @@ if __name__ == '__main__':
     if use_magic_v2:
         train_magic_v2 = base + 'train_magic_v2.csv'
         test_magic_v2 = base + 'test_magic_v2.csv'
+
+    if use_new:
+        train_new = base + 'train_features_5.csv'
+        test_new = base + 'test_features_5.csv'
+
+    if use_word_count:
+        train_word_count = base + 'train_word_count.pickle'
+        test_word_count = base + 'test_word_count.pickle'
+
+    if use_best:
+        train_best = './data/train_features_01584.csv'
+        test_best = './data/test_features_01584.csv'
 
     print("Loading files...")
     with open(train_path, 'rb') as f:
@@ -140,6 +156,49 @@ if __name__ == '__main__':
                 line = line.split(',')
                 test_magic_v2_data.append(int(line[1]))
 
+    if use_new:
+        train_new_data = []
+        with open(train_new) as f:
+            next(f)
+            for line in f:
+                line = line.split(',')
+                train_new_data.append([int(line[1]), int(line[2])])
+        test_new_data = []
+        with open(test_new) as f:
+            next(f)
+            for line in f:
+                line = line.split(',')
+                test_new_data.append([int(line[1]), int(line[2])])
+
+    if use_word_count:
+        with open(train_word_count, 'rb') as f:
+            train_word_count_data = pickle.load(f)
+        with open(test_word_count, 'rb') as f:
+            test_word_count_data = pickle.load(f)  
+
+    if use_best:
+        best_cols = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18]
+        train_best_data = []
+        with open(train_best) as f:
+            next(f)
+            for line in f:
+                line = line.strip().split(',')
+                for i in range(len(line)):
+                    if line[i] == '':
+                        line[i] = np.nan
+                line = [float(y) for x,y in enumerate(line) if x in best_cols]
+                train_best_data.append(line)
+        test_best_data = []
+        with open(test_best) as f:
+            next(f)
+            for line in f:
+                line = line.strip().split(',')
+                for i in range(len(line)):
+                    if line[i] == '':
+                        line[i] = np.nan
+                line = [float(y) for x,y in enumerate(line) if x in best_cols]
+                test_best_data.append(line)  
+
     print("Initialize features...")
 
     feature_dict = {'noun_sub': 0, 'verb_sub': 1, 'keyword_match': 2, 'word_difference': 3, 'noun_share': 4, 'verb_share': 5,'keyword_match_ratio': 6}
@@ -157,6 +216,8 @@ if __name__ == '__main__':
     labels = []
 
     for i, instance in enumerate(train_data):
+        if i in train_skip_row:
+            continue
         instance['features'] = [y for x,y in enumerate(instance['features']) if x in allow_index]
         if use_vsm:
             instance['features'].append(train_vsm_sim[i])
@@ -176,6 +237,12 @@ if __name__ == '__main__':
             instance['features'].append(train_lstm_data[i])
         if use_magic_v2:
             instance['features'].append(train_magic_v2_data[i])
+        if use_new:
+            instance['features'].extend(train_new_data[i])
+        if use_word_count:
+            instance['features'].extend(train_word_count_data[i])
+        if use_best:
+            instance['features'].extend(train_best_data[i])
         train_features.append(instance['features'])
         labels.append(instance['is_duplicate'])
 
@@ -185,7 +252,6 @@ if __name__ == '__main__':
     test_features = []
     for i, instance in enumerate(test_data):
         instance['features'] = [y for x,y in enumerate(instance['features']) if x in allow_index]
-        test_features.append(instance['features'])
         if use_vsm:
             instance['features'].append(test_vsm_sim[i])
         if use_magic:
@@ -204,6 +270,13 @@ if __name__ == '__main__':
             instance['features'].append(test_lstm_data[i])
         if use_magic_v2:
             instance['features'].append(test_magic_v2_data[i])
+        if use_new:
+            instance['features'].extend(test_new_data[i])
+        if use_word_count:
+            instance['features'].extend(test_word_count_data[i])
+        if use_best:
+            instance['features'].extend(test_best_data[i])
+        test_features.append(instance['features'])
     test_features = np.array(test_features)
 
     print("Initialize {} model...".format(method))
@@ -220,7 +293,7 @@ if __name__ == '__main__':
         model = AdaBoostClassifier(n_estimators=100)
         submit_path = 'submit_ada.csv'
     elif method == 'XGB':
-        model = xgboost.XGBClassifier(n_estimators=2000, nthread=18, max_depth=4)
+        model = xgboost.XGBClassifier(n_estimators=2200, nthread=18, max_depth=4)
         submit_path = 'submit_xgb.csv'
 
     # k-fold validation
@@ -261,6 +334,7 @@ if __name__ == '__main__':
     with open(submit_path, 'w') as f:
         f.write('test_id,is_duplicate\n')
         for i in range(len(test_data)):
+            # value = prob_result[i]
             value = normalize(prob_result[i])
             f.write(str(i) + ',' + str(value) + '\n')
 
